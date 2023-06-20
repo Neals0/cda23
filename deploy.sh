a174cb195b3b7e303973f58103683832e57b9e28
Sofiane <chahra.pro@hotmail.com>
2023-06-20 14:49:01 +0200

    modifs deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 # Mettre à jour le code source
 git pull
 # Construire l'image Docker
-docker build --no-cache -t image-application .
+docker build --no-cache -t loc-mns-front-v1 .
 # Arreter le conteneur existant
 docker stop conteneur-application
 # Supprimer le conteneur existant
 docker rm conteneur-application
 # Lancer un nouveau conteneur
-docker run -d --name=conteneur-application -p 4200:80 image-application
+docker run -d --name=angular-application -p 4200:80 loc-mns-front-v1
